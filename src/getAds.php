150005fd5d7ea6a3c502493cba239e2cdce858d1
juhajoki <juha.joki@sofiadigital.com>
2021-03-11 10:59:06 +0200

    http->https
    eliminate http links to improve interoperability in all environments

--- src/getAds.php
+++ src/getAds.php
@@ -2,19 +2,19 @@
 
 $ads = array(
 	/*
-	"http://refapp.hbbtv.org/videos/test/test1_5s.mp4",
-	"http://refapp.hbbtv.org/videos/test/test2_5s.mp4",
-	"http://refapp.hbbtv.org/videos/test/test3_5s.mp4",
+	"https://refapp.hbbtv.org/videos/test/test1_5s.mp4",
+	"https://refapp.hbbtv.org/videos/test/test2_5s.mp4",
+	"https://refapp.hbbtv.org/videos/test/test3_5s.mp4",
 	*/
-	"http://refapp.hbbtv.org/videos/test/test4_5s.mp4",
-	"http://refapp.hbbtv.org/videos/test/test1_15s.mp4",
-	"http://refapp.hbbtv.org/videos/test/test2_15s.mp4",
-	"http://refapp.hbbtv.org/videos/test/test3_15s.mp4",
-	"http://refapp.hbbtv.org/videos/test/test4_15s.mp4",
-	"http://refapp.hbbtv.org/videos/test/test1_30s.mp4" /* ,
-	"http://refapp.hbbtv.org/videos/test/test2_30s.mp4",
-	"http://refapp.hbbtv.org/videos/test/test3_30s.mp4",
-	"http://refapp.hbbtv.org/videos/test/test4_30s.mp4"
+	"https://refapp.hbbtv.org/videos/test/test4_5s.mp4",
+	"https://refapp.hbbtv.org/videos/test/test1_15s.mp4",
+	"https://refapp.hbbtv.org/videos/test/test2_15s.mp4",
+	"https://refapp.hbbtv.org/videos/test/test3_15s.mp4",
+	"https://refapp.hbbtv.org/videos/test/test4_15s.mp4",
+	"https://refapp.hbbtv.org/videos/test/test1_30s.mp4" /* ,
+	"https://refapp.hbbtv.org/videos/test/test2_30s.mp4",
+	"https://refapp.hbbtv.org/videos/test/test3_30s.mp4",
+	"https://refapp.hbbtv.org/videos/test/test4_30s.mp4"
 	*/
 	);
 	
